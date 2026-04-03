3c7338447cd93e6e6d258ee8875cd414951fad0f
system-temp-sf8 <saferrari8@gmail.com>
2026-04-03 17:20:48 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+// sw.js - Service Worker básico
+self.addEventListener('fetch', function(event) {
+  // Aquí podrías agregar lógica para que la web funcione sin internet, 
+  // pero por ahora lo dejamos vacío para que cumpla el requisito de instalación.
+});
